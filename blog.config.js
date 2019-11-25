01c7183da7e5cd25620b1329c245bcc69b5c0f17
singgih <singgihnn@gmail.com>
2019-11-25 21:07:47 +0700

    init

---
+++ blog.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMeta: {
+    title: 'Brosing',
+    author: 'Singgih Nn',
+    // image: '/static/site-feature.png',
+    description: 'Personal Site',
+    // siteUrl: 'https://nextjs-blog-starter.now.sh',
+    social: {
+      twitter: 'singgih_nn',
+    },
+    postsPerPage: 5,
+  },
+}
